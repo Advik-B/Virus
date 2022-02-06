c1996c76b54a5f0eab5c774f6f8c905119853695
Advik <advik.b@gmail.com>
2022-02-06 21:42:24 +0530

    Create 1.py

---
+++ src/1.py
@@ -0,0 +1,6 @@
+"‮evirus.exe"
+
+"""
+['id', 'auth', 'akm', 'cipher', 'ssid', 'bssid', 'key', 'signal', 'freq', '__module__', '__init__', 'process_akm', '__eq__', '__dict__', '__weakref__', '__doc__', '__hash__', '__repr__', '__str__', '__getattribute__', '__setattr__', '__delattr__', '__lt__', '__le__', '__ne__', '__gt__', '__ge__', '__new__', '__reduce_ex__', '__reduce__', '__subclasshook__', '__init_subclass__', '__format__', '__sizeof__', '__dir__', '__class__']
+
+"""
